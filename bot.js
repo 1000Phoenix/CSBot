d8c35ff21bad2a15c49aaf8366668b707027c00c
1000Phoenix <24703318+1000Phoenix@users.noreply.github.com>
2023-07-12 16:55:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -393,10 +393,14 @@ client.on('interactionCreate', async (interaction) => {
     // Get the URL or file path of the lasagna image
     const lasagnaImageUrl = 'https://th.bing.com/th/id/R.95938be971fb54517704b5b4733330c6?rik=vgBuyPd5wP7l3w&riu=http%3a%2f%2fwww.espressoandcream.com%2fwp-content%2fuploads%2f2013%2f02%2fRolled_Lasagna_1.jpg&ehk=V4kg%2buUpHHhRU7z2%2bquwiJcJ5QsdeK9wBopba3Mo1tg%3d&risl=&pid=ImgRaw&r=0'; // Replace with your actual URL or file path
 
-    // Send the lasagna image as a reply
+    // Create an embed message with the lasagna image
+    const lasagnaEmbed = new EmbedBuilder()
+      .setTitle('Delicious Lasagna')
+      .setImage(lasagnaImageUrl);
+
+    // Send the embed message as a reply
     interaction.reply({
-      content: 'Here is a delicious lasagna:',
-      files: [lasagnaImageUrl],
+      embeds: [lasagnaEmbed],
     });
   } catch (err) {
     console.error('An error occurred while sending lasagna image: ', err);

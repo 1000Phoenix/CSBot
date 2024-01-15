92d96262247fa15302ca53a63dbd2fe8e3bfbe20
1000Phoenix <24703318+1000Phoenix@users.noreply.github.com>
2024-01-15 10:45:47 +0000

    Added an echo command

---
+++ commands/echo.js
@@ -0,0 +1,33 @@
+const { EmbedBuilder } = require('discord.js');
+const config = require('../config.json');
+
+module.exports = {
+  name: 'echo',
+  description: 'Echoes the message provided by a moderator.',
+  options: [
+    {
+      name: 'message',
+      type: 'STRING',
+      description: 'The message to echo',
+      required: true,
+    },
+  ],
+  async execute(interaction) {
+    // Check if the user is a moderator
+    if (!config.moderators.includes(interaction.user.id)) {
+      return interaction.reply({
+        content: 'You do not have permission to use the echo command.',
+        ephemeral: true,
+      });
+    }
+
+    // Get the message to echo from the command options
+    const messageToEcho = interaction.options.getString('message');
+
+    // Reply with the message to echo
+    await interaction.reply({
+      content: messageToEcho,
+      ephemeral: false,
+    });
+  },
+};
